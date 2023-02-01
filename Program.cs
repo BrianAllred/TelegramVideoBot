c2ddaa9159112280696ab07e50e83de8f9946759
Brian Allred <brian.d.allred@gmail.com>
2023-01-31 18:54:56 -0600

    Remove old deps.

--- Program.cs
+++ Program.cs
@@ -17,18 +17,10 @@ internal class Program
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
         builder.Services.AddSingleton(config);
         builder.Services.AddHostedService<TelegramVideoBot.Workers.Telegram>();
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
 
         app.UseHttpsRedirection();
 
