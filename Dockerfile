3ed7d1f83cea44f78a7986b2bd0ec376cc05ad76
Brian Allred <brian.d.allred@gmail.com>
2023-12-01 14:12:30 -0600

    Update to dotnet 8

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 WORKDIR /src
 COPY TelegramVideoBot.csproj .
 RUN dotnet restore
@@ -8,7 +8,7 @@ RUN dotnet publish -c Release -o /app --self-contained false
 FROM linuxserver/ffmpeg as final
 WORKDIR /app
 COPY --from=build /app .
-RUN apt-get update -y
-RUN apt-get install -y python3-pip git aspnetcore-runtime-6.0
+COPY ./Scripts/install-dotnet8.sh .
+RUN bash install-dotnet8.sh
 RUN python3 -m pip install --force-reinstall git+https://github.com/yt-dlp/yt-dlp.git@release
 ENTRYPOINT [ "dotnet", "TelegramVideoBot.dll" ]
---
+++ Scripts/install-dotnet8.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set noninteractive
+export DEBIAN_FRONTEND=noninteractive
+
+# Update packages
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y wget
+
+# Get Ubuntu version
+declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
+
+# Download Microsoft signing key and repository
+wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+
+# Install Microsoft signing key and repository
+dpkg -i packages-microsoft-prod.deb
+
+# Clean up
+rm packages-microsoft-prod.deb
+
+# Install
+apt-get update -y
+apt-get install -y python3-pip git aspnetcore-runtime-8.0
